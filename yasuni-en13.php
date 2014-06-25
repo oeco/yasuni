38e672abcbfe3d741a993ee0fd32524acb7c917c
hvalois <hvalois@gmail.com>
2014-06-25 14:41:02 -0300

    Substituindo ecolab

--- yasuni-en13.php
+++ yasuni-en13.php
@@ -82,7 +82,7 @@
 
     <div class="boticario"><a href="http://www.grupoboticario.com.br" target="_blank"><img src="img/boticario.png" style="margin:20px auto;"></a></div>
 
-    <div class="ecolab"><a href="http://ecolab.oeco.org.br" target="_blank"><img src="img/ecolablogo.png" style="margin:20px auto;"></a></div>
+    <div class="infoamazonia"><a href="http://infoamazonia.org" target="_blank"><img src="img/infoamazonia.png" style="margin:20px auto;"></a></div>
 
 	<a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">Yasuni</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="http://yasuni.oeco.org.br/" property="cc:attributionName" rel="cc:attributionURL">((oeco))</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License</a>
 	</div>
